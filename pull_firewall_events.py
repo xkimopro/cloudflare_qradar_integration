a30b1e4e271db901e84051b8124296ba81617064
xkimopro <kimonaspro99@gmail.com>
2022-05-31 18:19:42 +0300

    Changes

--- pull_firewall_events.py
+++ pull_firewall_events.py
@@ -4,8 +4,6 @@ import os, json, ConfigParser,sys, ndjson, datetime \
 from threading import local
 from google.cloud import storage
 
-#VARIABLES
-
 CLOUDFLARE_BASE_DIR="/root/pull_cloudflare_events"
 LAST_EVENT_TIME_FILE="last_event_time.txt"
 config = ConfigParser.ConfigParser()
@@ -16,11 +14,9 @@ QRADAR_URL=config.get("cloudflare",'QRADAR_URL')
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"]=JSON_KEY_PATH
 
 
-#FUNCTIONS
-
-def list_blobs(bucket_name):
+def list_blobs(bucket_name,start_offset=None,max_results=None):
     storage_client = storage.Client()
-    blobs = storage_client.list_blobs(bucket_name)
+    blobs = storage_client.list_blobs(bucket_name,start_offset=start_offset, max_results=int(max_results) if max_results is not None else None)
     return [blob for blob in blobs]
 
 
@@ -35,22 +31,7 @@ def download_blob(bucket_name, source_blob_name, destination_file_name):
         )
     )
 
-def is_file(filename):
-    if filename.split('/')[1]!='':
-      return True
-    else:
-      return False
-
-def send_to_qradar(filename):
-    logfile=open(filename,'r')
-    events=logfile.readlines()
-    logfile.close()
-    for event in events:
-        requests.post(QRADAR_URL,data=event)
-
-def post_to_qradar(filename):
-   files={'files': open(filename,'rb')}
-   requests.post(QRADAR_URL,files=files)
+def is_file(filename): return filename.split('/')[1]!=''
 
 def write_to_json(filename):
    outfile=filename.strip('.log')+'.json'
@@ -61,50 +42,55 @@ def write_to_json(filename):
    outfile.close()
    infile.close()
 
-if  __name__=="__main__":
 
-    all_blobs=list_blobs(BUCKET)
-    file_index = 0
+def upload_remote_bucket_files(last_file_imported,max_results=None):
+    all_blobs=list_blobs(BUCKET,last_file_imported, max_results)
     for blob in all_blobs:
-        if is_file(blob.name): 
-            file_index+=1
-            local_filename="cloudflare_log"+str(file_index)+".ndjson"
-            download_blob(BUCKET,blob.name,local_filename)
-            with open(local_filename,"rw") as local_file:
-                try:
-                    # Try to parse the ndjson file
-                    event_list = ndjson.load(local_file)                
-                    while len(event_list) != 0:
-                        event = event_list.pop()
-                        # Testing 
-                        # requests.post("https://webhook.site/a2d08685-6d49-4819-b33a-eec11aa127c5", json=event )
-                        requests.post(QRADAR_URL, json=event )
+        if is_file(blob.name) and not blob.name == last_file_imported :             
+        
+            local_filename = blob.name.split("/")[-1].split(".")[0] + ".ndjson"
+
+            download_blob(BUCKET,blob.name, local_filename)
+            file_has_failed = parse_blob_and_post(local_filename)
+            if not file_has_failed:
+                with open("remember_filename.txt","w") as local_file:
+                    local_file.write(blob.name)
+
+def last_file_imported():
+    with open("remember_filename.txt","r") as local_file:
+        return local_file.readline()
+
+def parse_blob_and_post(local_filename):
+    with open(local_filename,"rw") as local_file:
+        # Try to parse the ndjson file
+        event_list = ndjson.load(local_file)          
+        file_has_failed = False      
+        while len(event_list) != 0:
+            event = event_list.pop()
+            resp = None
+            try: 
+               resp = requests.post(QRADAR_URL, json=event, timeout=5 )
+               if resp.status_code != 200:
+                    print("File has failed to post")
+                    file_has_failed = True
+                    break
+            except Exception as e:
+                print("Exception! File has failed to post")
+                file_has_failed = True
+                break
+            
+
+        print("File ingested by QRadar")
+       
+    os.remove(local_filename)
+    return file_has_failed
 
 
-                    print("Finished")
 
 
+if  __name__=="__main__":
+    if len(sys.argv) != 2 : max_results = None 
+    else: max_results = sys.argv[1]
+    last_file_imported = last_file_imported()
+    upload_remote_bucket_files(last_file_imported,max_results)
 
-                except Exception as e:
-                    print(e.message, e.args)
-            os.remove(local_filename)
-
-    
-#   with open(LAST_EVENT_TIME_FILE,"r") as event_time:
-#       last_event_time=event_time.readline().rstrip()
-#   print int(last_event_time)
-#   print cloudflare_logs
-#   i=0
-#   for cloudflare_log in cloudflare_logs:
-#     i=i+1
-#     print cloudflare_log.name
-#     filename="cloudflare_log"+str(i)+".log"
-#     #download_blob(BUCKET,logs_list[1],filename)
-#     #print logs_list[i]
-#     if is_file(cloudflare_log.name):
-#       download_blob(BUCKET,cloudflare_log.name,filename)
-#       post_to_qradar(filename)
-#       #print type(filename)
-#       event_time=open(LAST_EVENT_TIME_FILE,"w")
-#       event_time.write(str(cloudflare_log.generation))
-#       event_time.close()
